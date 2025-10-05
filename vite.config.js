ca0833181e35cfcaf883dd804aa082069ad91b7c
d1vij <77372157+d1vij@users.noreply.github.com>
2025-10-05 17:32:18 +0000

    Upload

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/sharktank-website/" : "/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        "login-user": "/src/pages/login/user/index.html",
+        "login-advocate": "/src/pages/login/advocate/index.html",
+        "chat-ai": "/src/pages/chat/ai/index.html",
+        "chat-mediation": "/src/pages/chat/mediation/index.html",
+      },
+    },
+  },
+}));
